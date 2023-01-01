dae43c4233a28561a3f7630cfdc1d9bb96cd5132
denystreltsov <deny.streltsov@protonmail.com>
2023-01-01 13:16:16 +0000

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,17 @@
 
 export default function Home() {
-  return <div>hello world</div>;
-}
+    return(
+        <div className='h-screen w-screen flex flex-col justify-center items-center relative'>
+          <div className='text-2xl text-center'>Which pokemon is Rounder</div>
+          <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
+            <div className='w-16 h-16 bg-red-200'></div>
+            <div className='p-8'>Vs</div>
+            <div className='w-16 h-16 bg-red-200'></div>
+          </div>
+        </div>
+
+
+
+
+    )
+  }
