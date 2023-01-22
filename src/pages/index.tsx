22f0ebd2938d8703ee9324659d8b4cebaa027778
denystreltsov <deny.streltsov@protonmail.com>
2023-01-22 19:37:20 +0000

    adding prisma + railway.app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { trpc } from "@/utils/trpc";
 import { useState, useEffect } from "react";
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import type { inferProcedureOutput } from "@trpc/server";
+import type { FC } from "react";
+const btn = "flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-1 text-base text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 
 export default function Home() {
   const [ids, setIds] = useState([0, 0] as [number, number]);
@@ -17,20 +20,22 @@ export default function Home() {
 
   if (status === "error") return error;
 
-  if (status === "success") {
+  const voteForRoundest = (selected : number) =>{
+      setIds(getOptionsForVote());
   }
 
-  //<img src={data[0]?.sprites.front_default} alt="" />
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
       <div className="text-2xl text-center">Which pokemon is Rounder</div>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200">
-          <img src={data[0].sprites.front_default as string | undefined}  alt="" />
+        <div className="w-64 h-64 flex flex-col items-center bg-red-200">
+          <img className="w-full h-full object-cover"  src={data[0].sprites.front_default as string | undefined}  alt="" />
+          <button className={btn} onClick={()=> {voteForRoundest(1)}}>Roundest</button>
         </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200">
-          <img src={data[1]?.sprites.front_default as string | undefined} alt="" />
+        <div className="w-64 h-64 flex flex-col items-center bg-red-200">
+          <img className="w-full h-full object-cover" src={data[1]?.sprites.front_default as string | undefined} alt="" />
+          <button className={btn}  onClick={()=> {voteForRoundest(2)}}>Roundest</button>
         </div>
       </div>
     </div>
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,11 +1,12 @@
 import {router} from "../trpc";
 import { hello_world } from "./hello_world"; 
 import { pokemon_router } from "./pokemon";
-
+import { inferProcedureOutput } from "@trpc/server";
 export const appRouter = router({
     hello: hello_world,
     pokemon: pokemon_router,
 })
+
 
 
 export type AppRouter = typeof appRouter;
